416639f8a66d45ca213022eba18c5b6d709c376e
ashraf-learn-cs <ashraf.learn.cs@gmail.com>
2024-12-02 06:16:23 -0500

    updates

--- problems/if_then_else.py
+++ problems/if_then_else.py
@@ -10,12 +10,13 @@
 #  If  is even and greater than 20, print Not Weird
 #-------------------------------------------------------------------------------------------------
 def if_then_else(n: int)-> None:
-    if (n % 2 != 0):  # is odd
-        print("Weird")
-    else:
-        if n >= 2 and n <= 5:
-            print("Not Weird")
-        elif n >= 6 and n <= 20:
-            print("Weird")
-        elif n > 20:
-            print("Not Weird")
+    #if (n % 2 != 0):  # is odd
+    #    print("Weird")
+    #else:
+    #    if n >= 2 and n <= 5:
+    #        print("Not Weird")
+    #    elif n >= 6 and n <= 20:
+    #        print("Weird")
+    #    elif n > 20:
+    #        print("Not Weird")
+    ...
